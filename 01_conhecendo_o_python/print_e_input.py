584bc90c2bde49eab27c3392afff140f287ae9bb
VitoriaCamposRosa <vitoriacamposrosa28@gmail.com>
2025-03-12 22:33:51 -0300

    Alterações códigos python

--- 01_conhecendo_o_python/print_e_input.py
+++ 01_conhecendo_o_python/print_e_input.py
@@ -1,2 +1,21 @@
 name = input("Qual seu nome?")
 print("Olá,", name)
+
+name = input("Qual seu nome?")
+sobrenome = input("Qual seu sobrenome?")
+print("Olá,", name, sobrenome)
+
+idade = input("Qual sua idade?")
+print("Olá,", idade)
+
+ano_nasc = input("Qual seu ano de nascimento?")
+idade = 2025 - ano_nasc
+print("Sua idade é", idade "?")
+if (idade )
+
+ano = int(input("Ditige um ano:"))
+print("O ano digitado foi:", ano)
+if ano % 4 == 0:
+print("Ano é bissexto")
+else
+print("O ano não é bissexto")
