c7f8e1281e8b151f34f437158e62c1e1c0f4ff75
VitoriaCamposRosa <vitoriacamposrosa28@gmail.com>
2025-03-12 21:41:21 -0300

    Alterações Média

--- 01_conhecendo_o_python/media_idades.py
+++ 01_conhecendo_o_python/media_idades.py
@@ -4,3 +4,13 @@ idade_C = 21
 media = (idade_B + idade_A + idade_C) / 3
 
 print("A média é", media)
+
+idade_A = 18
+idade_B = 19
+idade_C = 21
+idade_D = 32
+idade_E = 12
+media = (idade_B + idade_A + idade_C + idade_D + idade_E) / 5
+
+print("A média é", media)
+
