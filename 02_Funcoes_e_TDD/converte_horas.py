54af2e460e10a2c0337e9f15d2f91883d86f3b4b
VitoriaCamposRosa <vitoriacamposrosa28@gmail.com>
2025-03-17 22:18:58 -0300

    Atividades Funções

--- 02_Funcoes_e_TDD/converte_horas.py
+++ 02_Funcoes_e_TDD/converte_horas.py
@@ -5,7 +5,10 @@ def minuto_para_segundo(valor):
     return valor * 60
 
 def hora_para_segundo(valor):
-   return minuto_para_segundo(valor) * 60
+   return minuto_para_segundo(hora_para_minuto(valor))
+
+def dia_para_segundos(valor):
+    return (minuto_para_segundo(hora_para_minuto(valor))) * 24
 
 assert hora_para_minuto(0) == 0
 assert hora_para_minuto(1) == 60
@@ -18,5 +21,9 @@ assert minuto_para_segundo(2) == 120
 assert hora_para_segundo(0) == 0
 assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
 assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
+
+assert dia_para_segundos(0) == 0
+assert dia_para_segundos(1) == 86400
+assert dia_para_segundos(2) == 172800
 
 print("Terminou com sucesso!")
