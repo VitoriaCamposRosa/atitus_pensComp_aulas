e0e0cc64652afe93b7a5dffbc9ad4ae2de24f3c6
VitoriaCamposRosa <vitoriacamposrosa28@gmail.com>
2025-03-19 19:25:35 -0300

    Atividade 1 - Funções

--- 02_Funcoes_e_TDD/converte_horas.py
+++ 02_Funcoes_e_TDD/converte_horas.py
@@ -8,23 +8,23 @@ def hora_para_segundo(valor):
    return minuto_para_segundo(hora_para_minuto(valor))
 
 def dia_para_segundos(valor):
-    return minuto_para_segundo(hora_para_minuto(valor)) * 24 #Verificar outras respostas
+    return minuto_para_segundo(hora_para_minuto(valor)) * 24 
 
 def test():
-assert hora_para_minuto(0) == 0
-assert hora_para_minuto(1) == 60
-assert hora_para_minuto(2) == 120
+    assert hora_para_minuto(0) == 0
+    assert hora_para_minuto(1) == 60
+    assert hora_para_minuto(2) == 120
 
-assert minuto_para_segundo(0) == 0
-assert minuto_para_segundo(1) == 60
-assert minuto_para_segundo(2) == 120
+    assert minuto_para_segundo(0) == 0
+    assert minuto_para_segundo(1) == 60
+    assert minuto_para_segundo(2) == 120
 
-assert hora_para_segundo(0) == 0
-assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
-assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
+    assert hora_para_segundo(0) == 0
+    assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
+    assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
 
-assert dia_para_segundos(0) == 0
-assert dia_para_segundos(1) == 86400
-assert dia_para_segundos(2) == 172800
+    assert dia_para_segundos(0) == 0
+    assert dia_para_segundos(1) == 86400
+    assert dia_para_segundos(2) == 172800
 
 print("Terminou com sucesso!")
--- 02_Funcoes_e_TDD/converte_metros.py
+++ 02_Funcoes_e_TDD/converte_metros.py
@@ -6,12 +6,12 @@ def centimetro_para_metro(valor):
     return valor / 100
 
 def test():
-assert metro_para_centimetro(1) == 100
-assert metro_para_centimetro(10) == 1000
-assert metro_para_centimetro(0) == 0
+    assert metro_para_centimetro(1) == 100
+    assert metro_para_centimetro(10) == 1000
+    assert metro_para_centimetro(0) == 0
 
-assert centimetro_para_metro(100) == 1
-assert centimetro_para_metro(1000) == 10
-assert centimetro_para_metro(0) == 0
+    assert centimetro_para_metro(100) == 1
+    assert centimetro_para_metro(1000) == 10
+    assert centimetro_para_metro(0) == 0
 
 print("Terminou com sucesso!")
