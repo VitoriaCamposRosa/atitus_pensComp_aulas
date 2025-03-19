a51cd4e10e150f1a69a812b1735771305ee34b1f
VitoriaCamposRosa <vitoriacamposrosa28@gmail.com>
2025-03-19 19:19:16 -0300

    Alterações das funções

--- 02_Funcoes_e_TDD/converte_horas.py
+++ 02_Funcoes_e_TDD/converte_horas.py
@@ -10,6 +10,7 @@ def hora_para_segundo(valor):
 def dia_para_segundos(valor):
     return (minuto_para_segundo(hora_para_minuto(valor))) * 24 #Verificar outras respostas
 
+def test():
 assert hora_para_minuto(0) == 0
 assert hora_para_minuto(1) == 60
 assert hora_para_minuto(2) == 120
