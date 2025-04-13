62a6b7f2fa5e281e1b4e1e52e8635111778ac996
VitoriaCamposRosa <vitoriacamposrosa28@gmail.com>
2025-04-13 16:42:00 -0300

    LISTA 5

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -1,5 +1,5 @@
 def baskhara(a, b, c):
-   if a == 0:
+    if a == 0:
         return None
     
     delta = (b ** 2) - 4 * a * c
@@ -15,7 +15,7 @@ def baskhara(a, b, c):
         return sorted([x1, x2])
 
 def test():
-    assert baskhara(1, -3, 2) == [2, 1]
+    assert baskhara(1, -3, 2) == [1, 2]
     assert baskhara(2, 3, -2) == [-2, 0.5]
     assert baskhara(1, -5, 6) == [2, 3]
     assert baskhara(1, -7, 10) == [2, 5]
