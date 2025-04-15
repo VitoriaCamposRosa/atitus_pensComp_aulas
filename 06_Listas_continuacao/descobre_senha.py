0bd8aa5c7638b3ae6d3fcd6497ecf509d686dece
VitoriaCamposRosa <vitoriacamposrosa28@gmail.com>
2025-04-15 10:48:09 -0300

    Lista 6

--- 06_Listas_continuacao/descobre_senha.py
+++ 06_Listas_continuacao/descobre_senha.py
@@ -1,4 +1,4 @@
-def descobre_senha_secreta():
+def descobre_senha_secreta(senha_secreta, tentativas):
     print("Bem vindo ao Jogo Descobre Senha! Será que você consegue advinhar?")
     print()
 
@@ -6,25 +6,28 @@ def descobre_senha_secreta():
     tentativas = 0
 
     while True:
-        descobre_senha = int(input("Digite um número de 1 a 10 que você supõe que é a senha: "))
-        print()
+        try:
+            descobre_senha = int(input("Digite um número de 1 a 10 que você supõe que é a senha: "))
+        except ValueError:
+            print("Entrada inválida. Por favor, digite um número inteiro de 1 a 10.")
+            print()
+            continue
 
         if descobre_senha < 1 or descobre_senha > 10:
             print("É um número de 1 a 10. Digite um novo número")
-        else:
-            tentativas += 1
+            print()
+            continue
 
-            if descobre_senha == senha_secreta:
-                print("Parabéns! Você descobriu a senha na primeira tentativa.")
-                break
-            else: 
-                print("Ah não, está incorreto.Tente novamente")
-                print()
-
-        while descobre_senha != senha_secreta: 
         tentativas += 1
-        descobre_senha = int(input("Tente adivinhar a senha: "))
-        print()
-        print("Parabéns! Você descobriu a senha. O número de tentativas foram ", tentativas)
-        
+
+        if descobre_senha == senha_secreta:
+            if tentativas == 1:
+                print("Parabéns! Você descobriu a senha na primeira tentativa.")
+            else:
+                print(f"Parabéns! Você descobriu a senha. O número de tentativas foi {tentativas}.")
+            break
+        else:
+            print("Ah não, está incorreto. Tente novamente")
+            print()
+
 descobre_senha_secreta()
