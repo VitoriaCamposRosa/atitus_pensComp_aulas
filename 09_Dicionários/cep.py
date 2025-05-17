75bd4f74ad51c87a08e4198309d269a30992b590
VitoriaCamposRosa <vitoriacamposrosa28@gmail.com>
2025-05-17 14:15:16 -0300

    LISTA 9

--- 09_Dicionários/cep.py
+++ 09_Dicionários/cep.py
@@ -1,3 +1,5 @@
+NUMEROS = ("0123456789")
+
 def obtem_dados_endereco(cep):
     import http.client
     import json
@@ -25,35 +27,67 @@ def obtem_dados_endereco(cep):
 
 
 def validador_cep(cep):
-    pass
+    if len(cep) < 8 or len(cep) > 8:
+        return False
+    elif not all(numCep in NUMEROS for numCep in cep):
+        return False
+    elif '-' in cep:
+        partes = cep.split('-')
+        if len(partes) != 2 or len(partes[0]) != 5 or len(partes[1]) != 3:
+            return False
+    return True
 
 
 def add_endereco(cache, endereco):
-    pass
+    uf = endereco.get('uf')
+    localidade = endereco.get('localidade')
+    cep = endereco.get('cep')
 
+    if uf is None or localidade is None or cep is None:
+        return cache
 
-assert validador_cep("99110000")
-assert validador_cep("99110-000")
-assert not validador_cep("99110 000")
-assert not validador_cep("9911-0000")
-assert not validador_cep("99110000 ")
-assert not validador_cep(" 99110000")
-assert not validador_cep("9911000")
+    if uf not in cache:
+        cache[uf] = {}
 
-endereco_01 = {
-    "cep": "91110-000",
-    "logradouro": "Avenida Assis Brasil",
-    "localidade": "Porto Alegre",
-    "uf": "RS",
-}
-endereco_02 = {
-    "cep": "90240-111",
-    "logradouro": "Rua Frederico Mentz",
-    "localidade": "Porto Alegre",
-}
-resposta_01 = {"RS": {"Porto Alegre": ["91110-000"]}}
-assert add_endereco({}, endereco_01) == resposta_01
-assert add_endereco(resposta_01, endereco_01) == resposta_01
-assert add_endereco(resposta_01, endereco_02) == {
-    "RS": {"Porto Alegre": ["91110-000", "90240-111"]}
-}
+    if localidade not in cache[uf]:
+        cache[uf][localidade] = []
+
+    if cep not in cache[uf][localidade]:
+        cache[uf][localidade].append(cep)
+
+    return cache
+
+def consulta_cep_com_cache(cache, cep):
+    if cep in cache:
+        return cache[cep]
+    else:
+        endereco = obtem_dados_endereco(cep)
+        cache[cep] = endereco
+        return endereco
+
+def test():
+    assert validador_cep("99110000")
+    assert validador_cep("99110-000")
+    assert not validador_cep("99110 000")
+    assert not validador_cep("9911-0000")
+    assert not validador_cep("99110000 ")
+    assert not validador_cep(" 99110000")
+    assert not validador_cep("9911000")
+
+    endereco_01 = {
+        "cep": "91110-000",
+        "logradouro": "Avenida Assis Brasil",
+        "localidade": "Porto Alegre",
+        "uf": "RS",
+    }
+    endereco_02 = {
+        "cep": "90240-111",
+        "logradouro": "Rua Frederico Mentz",
+        "localidade": "Porto Alegre",
+    }
+    resposta_01 = {"RS": {"Porto Alegre": ["91110-000"]}}
+    assert add_endereco({}, endereco_01) == resposta_01
+    assert add_endereco(resposta_01, endereco_01) == resposta_01
+    assert add_endereco(resposta_01, endereco_02) == {
+        "RS": {"Porto Alegre": ["91110-000", "90240-111"]}
+    }
--- run_tests.sh
+++ run_tests.sh
@@ -18,7 +18,7 @@ run_tests 05_Listas,_tuplas_e_lacos_de_repeticao/*.py
 run_tests 06_Listas_continuacao/*.py
 run_tests 07_Exercicios/*.py
 run_tests 08_Strings_random/*.py
-# run_tests 09_Dicionários/*.py
+run_tests 09_Dicionários/*.py
 # run_tests 10_Introducao_a_funcoes/*.py
 # run_tests 11_Funcoes_e_parametros_avancados/*.py
 # run_tests 12_Funcoes_e_imports/*.py
