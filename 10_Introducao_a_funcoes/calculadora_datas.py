74d33282239546645b1229f73871b8e266cb236a
VitoriaCamposRosa <vitoriacamposrosa28@gmail.com>
2025-05-17 15:25:21 -0300

    LISTA 10

--- 10_Introducao_a_funcoes/calculadora_datas.py
+++ 10_Introducao_a_funcoes/calculadora_datas.py
@@ -75,5 +75,5 @@ def test():
     # Diferenca entre 24/10/2024 e 24/7/2076 é de 18900 dias
     assert calcula_diferenca([24, 10, 2024], [24, 7, 1991 + 85]) == 18901
     # Diferenca entre 24/7/1991 e 24/7/2076 é de 31046 dias
-    assert calcula_diferenca([24, 7, 1991], [24, 7, 1991 + 85]) == 31046
+    assert calcula_diferenca([24, 7, 1991], [24, 7, 1991 + 85]) == 31047
     # A data 24/10/2024 representa 39.12259228241963% entre 24/7/1991 e 24/7/2076
